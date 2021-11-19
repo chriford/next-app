b22c1a102b06eaf2d0d5d9a6beead1587f4566c6
Chriford Siame <siamechrif@gmail.com>
2021-11-19 19:14:02 +0200

    update Employee Detail page

--- pages/posts/view.js
+++ pages/posts/view.js
@@ -15,20 +15,44 @@ const employeeInfo = () => {
         };
         getData();
     })
+
     return (
         <>
             <main>
-                <div className="bg-blue-100 rounded-xl p-8">
-                    <h1>Employee View</h1>
-                    <h1>Employee View</h1>
+                  <div className="bg-white p-2">
+                    <a>home</a>
+                  </div>
+                <div className="bg-gray-800 rounded-0 p-8 text-opacity-300 m-0">
+                    <div className="text-white text-4xl">
+                      <h1 className="flex space-x-4 text-5xl text-center underline">Employee Detail</h1>
+                      <div className="inline-block no-underline hover:bg-blue-500 hover:text-black rounded-xl ml-2 mt-2 p-2 bg-blue-600"
+                      ><h2>Fast Writer</h2>
 
-                    <h1>Employee View</h1>
-                    <h1>Employee View</h1>
-                    <h1>Employee View</h1>
-                    <h1>Employee View</h1>
-                    <h1>Employee View</h1>
+                      </div>
+                      <div className="inline-block no-underline hover:bg-blue-500 hover:text-black rounded-xl ml-2 mt-2 p-2 bg-blue-600"
+                      ><h2>Fast Writer</h2>
+                      <p className="text-2xl">
+                        Hello world
+                      </p>
 
-                    
+                      </div>
+                      <div className="inline-block no-underline hover:bg-blue-500 hover:text-black rounded-xl ml-2 mt-2 p-2 bg-blue-600"
+                      ><h2>Fast Writer</h2>
+
+                      </div>
+                      <div className="inline-block no-underline hover:bg-blue-500 hover:text-black rounded-xl ml-2 mt-2 p-2 bg-blue-600"
+                      ><h2>Fast Writer</h2>
+
+                      </div>
+                      <div className="inline-block no-underline hover:bg-blue-500 hover:text-black rounded-xl ml-2 mt-2 p-2 bg-blue-600"
+                      ><h2>Fast Writer</h2>
+
+                      </div>
+                      <div className="inline-block no-underline hover:bg-blue-500 hover:text-black rounded-xl ml-2 mt-2 p-2 bg-blue-600"
+                      ><h2>Fast Writer</h2>
+
+                      </div>
+                    </div>
                 </div>
             </main>
         </>
